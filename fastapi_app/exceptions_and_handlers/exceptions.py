cc411a3dddf39d0b9325d390bc76f18820af206b
Maksim Pislar <pislarmaxim@gmail.com>
2025-10-06 17:34:09 +0300

    Remove unused argument in exceptions. Add headers argument in InvalidCredentialsException

--- fastapi_app/exceptions_and_handlers/exceptions.py
+++ fastapi_app/exceptions_and_handlers/exceptions.py
@@ -2,18 +2,35 @@ from fastapi import HTTPException, status
 
 
 class UserNotFoundException(HTTPException):
-    def __init__(self, detail: str, error_code: str, status_code: status.HTTP_404_NOT_FOUND):
-        self.error_code = error_code
+    def __init__(
+            self,
+            detail: str,
+            status_code: status.HTTP_404_NOT_FOUND
+    ):
         super().__init__(detail=detail, status_code=status_code)
 
 
 class InvalidUserDataException(HTTPException):
-    def __init__(self, detail: str, error_code: str, status_code: status.HTTP_409_CONFLICT):
-        self.error_code = error_code
+    def __init__(
+            self,
+            detail: str,
+            status_code: status.HTTP_409_CONFLICT
+    ):
         super().__init__(detail=detail, status_code=status_code)
 
 
 class InvalidCredentialsException(HTTPException):
-    def __init__(self, detail: str, error_code: str, status_code: status.HTTP_401_UNAUTHORIZED):
-        self.error_code = error_code
-        super().__init__(detail=detail, status_code=status_code)
+    def __init__(
+            self,
+            detail: str,
+            status_code: status.HTTP_401_UNAUTHORIZED,
+            headers: dict = None
+    ):
+        if headers is None:
+            headers = {"WWW-Authenticate": "Bearer"}
+
+        super().__init__(
+            detail=detail,
+            status_code=status_code,
+            headers=headers
+        )

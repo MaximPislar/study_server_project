cf8893f708643b1e1cbcfe1a7e050a5f9cb3fc66
Maksim Pislar <pislarmaxim@gmail.com>
2025-10-15 15:01:17 +0300

    Add functional to login endpoint

--- fastapi_app/routers/security_router.py
+++ fastapi_app/routers/security_router.py
@@ -1,11 +1,15 @@
+from datetime import timedelta
 from typing import Annotated
 
 from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from fastapi_app.core import settings
 from fastapi_app.database import db_helper
 from fastapi_app.schemas.token import Token
+from fastapi_app.exceptions_and_handlers import InvalidCredentialsException
+from fastapi_app.auth import authenticate_user, create_access_token
 
 router = APIRouter(
     prefix="/security",
@@ -18,4 +22,22 @@ async def login(
         creds: Annotated[OAuth2PasswordRequestForm, Depends()],
         session: Annotated[AsyncSession, Depends(db_helper.session_getter)]
 ):
-    pass
+    user = await authenticate_user(
+        session=session,
+        username=creds.username,
+        password=creds.password
+    )
+    if not user:
+        raise InvalidCredentialsException(
+            detail="Invalid username or password"
+        )
+
+    user_id = str(user.id)
+
+    # тут мы создаём jwt токен
+    access_token_expires = timedelta(minutes=settings.auth.access_token_expire_minutes)
+    access_token = create_access_token(
+        data={"sub": user_id}, expires_delta=access_token_expires
+    )
+
+    return Token(access_token=access_token)

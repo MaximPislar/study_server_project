107c4c6b848a44069086d41d94942d5f5e28f651
Maksim Pislar <pislarmaxim@gmail.com>
2025-10-15 14:59:43 +0300

    Change algorithm to RS256

--- fastapi_app/core/config.py
+++ fastapi_app/core/config.py
@@ -21,7 +21,7 @@ class DatabaseConfig(BaseModel):
 class AuthJWT(BaseModel):
     privat_key_path: Path = BASE_DIR / "private.pem"
     public_key_path: Path = BASE_DIR / "public.pem"
-    algorithm: str = "HS256"
+    algorithm: str = "RS256"
     access_token_expire_minutes: int = 15
 
 

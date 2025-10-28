f4e47eec3b21664c16a249e74b2a7a15f4fa86ed
Maksim Pislar <pislarmaxim@gmail.com>
2025-10-28 16:20:00 +0200

    add secured endpoint /me

--- fastapi_app/routers/security_router.py
+++ fastapi_app/routers/security_router.py
@@ -4,9 +4,12 @@ from typing import Annotated
 from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
 
+from fastapi_app.auth.utils import get_current_active_user_from_token
 from fastapi_app.core import settings
-from fastapi_app.database import db_helper
+from fastapi_app.database import db_helper, User
+from fastapi_app.schemas import UserResponse, ErrorResponseModel
 from fastapi_app.schemas.token import Token
 from fastapi_app.exceptions_and_handlers import InvalidCredentialsException
 from fastapi_app.auth import authenticate_user, create_access_token
@@ -41,3 +44,22 @@ async def login(
     )
 
     return Token(access_token=access_token)
+
+
+@router.get(
+    path='/me',
+    response_model=UserResponse,
+    status_code=status.HTTP_200_OK,
+    description="Returns information about the logged-in user",
+    responses={
+        status.HTTP_200_OK: {"model": UserResponse},
+        status.HTTP_401_UNAUTHORIZED: {"model": ErrorResponseModel},
+        status.HTTP_409_CONFLICT: {"model": ErrorResponseModel}
+}
+
+    # TODO довести модели ошибок до ума
+)
+async def get_user(
+        current_user: Annotated[User, Depends(get_current_active_user_from_token)]
+):
+    return current_user

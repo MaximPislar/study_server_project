2d6cc3ec83d4a62a1cce0e91af7f94317a4ab198
Maksim Pislar <pislarmaxim@gmail.com>
2025-11-03 20:00:44 +0200

    Create refresh_request, file and model. Add to __init__

--- fastapi_app/schemas/__init__.py
+++ fastapi_app/schemas/__init__.py
@@ -1,11 +1,13 @@
 from .user import UserRegistration, UserResponse
 from .exceptions import ErrorResponseModel
 from .token import Token
+from .refresh_request import RefreshRequest
 
 
 __all__ = [
     "UserRegistration",
     "UserResponse",
     "ErrorResponseModel",
-    "Token"
+    "Token",
+    "RefreshRequest"
 ]

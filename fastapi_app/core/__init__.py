ed9356ecb296db474b5c7a0aa5098964c018ba2a
Maksim Pislar <pislarmaxim@gmail.com>
2025-11-03 20:30:29 +0200

    ADD constant TOKEN_TYPE_FIELD

--- fastapi_app/core/__init__.py
+++ fastapi_app/core/__init__.py
@@ -1,9 +1,10 @@
 from .config import settings
-from .constants import ACCESS_TOKEN, REFRESH_TOKEN
+from .constants import ACCESS_TOKEN, REFRESH_TOKEN, TOKEN_TYPE_FIELD
 
 
 __all__ = [
     "settings",
     "ACCESS_TOKEN",
-    "REFRESH_TOKEN"
+    "REFRESH_TOKEN",
+    "TOKEN_TYPE_FIELD"
 ]
--- fastapi_app/core/constants.py
+++ fastapi_app/core/constants.py
@@ -1,3 +1,4 @@
 # jwt types
+TOKEN_TYPE_FIELD = "type"
 ACCESS_TOKEN = "access"
 REFRESH_TOKEN = "refresh"

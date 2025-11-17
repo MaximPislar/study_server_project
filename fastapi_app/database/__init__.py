70b2d462e782126a76ec87e173bf130101ba7146
Maksim Pislar <pislarmaxim@gmail.com>
2025-11-17 17:43:05 +0200

    chore: update __init__.py

--- fastapi_app/database/__init__.py
+++ fastapi_app/database/__init__.py
@@ -1,8 +1,9 @@
 from .db_helper import db_helper
-from .models import User, Base
+from .models import User, Base, RefreshToken
 
 __all__ = [
     "db_helper",
     "User",
-    "Base"
+    "Base",
+    "RefreshToken"
 ]
